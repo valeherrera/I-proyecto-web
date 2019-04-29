023573152e4f3502479c0ccc81c60a5315f5ca69
Valeria Herrera <37224240+valeherrera@users.noreply.github.com>
2019-04-28 21:05:43 -0600

    proyecto terminado

---
+++ codigo/main.js
@@ -0,0 +1,379 @@
+var datos = [{ "rdia": "2019-01-01", "rporcentaje": "10", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-02", "rporcentaje": "20", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-03", "rporcentaje": "30", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-04", "rporcentaje": "40", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-05", "rporcentaje": "50", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-06", "rporcentaje": "60", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-07", "rporcentaje": "70", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-08", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-09", "rporcentaje": "90", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-10", "rporcentaje": "100", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-11", "rporcentaje": "110", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-12", "rporcentaje": "120", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-13", "rporcentaje": "130", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-14", "rporcentaje": "140", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-15", "rporcentaje": "150", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-16", "rporcentaje": "160", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-17", "rporcentaje": "170", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-18", "rporcentaje": "180", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-19", "rporcentaje": "190", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-20", "rporcentaje": "120", "ridnombramientos": "{41433,41434}" },
+{ "rdia": "2019-01-21", "rporcentaje": "130", "ridnombramientos": "{41433,41434}" },{ "rdia": "2019-01-22", "rporcentaje": "125", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-23", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-24", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-25", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-26", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-27", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-28", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-29", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-30", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-01-31", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-01", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-02", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-03", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-04", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-05", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-06", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-07", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-08", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-09", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-10", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-11", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-12", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-13", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-14", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-15", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-16", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-17", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-18", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-19", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-20", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-21", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-22", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-23", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-24", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-25", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-26", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-27", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-02-28", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-01", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-02", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-03", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-04", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-05", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-06", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-07", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-08", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-09", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-10", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-11", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-12", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-13", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-14", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-15", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-16", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-17", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-18", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-19", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-20", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-21", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-22", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-23", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-24", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-25", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-26", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-27", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-28", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-29", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-30", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }, { "rdia": "2019-03-31", "rporcentaje": "80", "ridnombramientos": "{41433,41434}" }]
+
+var datosDiasMesesAnnos = [];
+/**
+ * 
+ * @param {string} fechaInicio 
+ * @param {string} fechaFinal 
+ * @param {string} codigoPlaza 
+ */
+
+/**Funcion para la grafica en el tipo diario */
+
+
+function direccionamiento(funcion) {
+    
+    var FI = document.getElementById('fechaInicio').value;
+    var FF = document.getElementById('fechaFinal').value;
+    var plaza = document.getElementById('plaza').value;
+
+    if(funcion=='datos'){
+    
+        pintarGrafico(datos);
+    
+    }    
+    else if (FI == '' || FF == '' || plaza =='') {
+        window.alert("Faltan datos por ingresar");
+        
+    }
+    else {
+        if (funcion == 'dias') {
+            diario(FI, FF, plaza);
+        } else {
+            meses_annos(FI, FF, plaza);
+        }
+    }
+    
+    
+}
+
+function diario(fechaInicio, fechaFinal, codigoPlaza) {
+
+    var dias = [];
+
+    for (var index = 0; index < datos.length; index++) {
+
+        var repl = datos[index].ridnombramientos.replace(/[{}]/g, "").split(",");
+
+        if (repl[0] == codigoPlaza || repl[1] == codigoPlaza){
+
+            var fecha = Date.parse(datos[index].rdia);
+            var fechaInicioE = Date.parse(fechaInicio);
+            var fechaFinalE = Date.parse(fechaFinal);
+
+            if (fecha >= fechaInicioE && fecha <= fechaFinalE) {
+                dias.push({ "rporcentaje": datos[index].rporcentaje, "rdia": datos[index].rdia })
+
+            }else if(fechaFinal < fechaInicio){
+               alert("Las fechas no coinciden");
+               break;
+            }
+        } 
+    }
+
+    pintarGrafico(dias);
+    
+    
+}
+
+/**
+ * 
+ * @param {string} fechaInicio 
+ * @param {string} fechaFinal 
+ * @param {string} codigoPlaza 
+ */
+/* Funcion para la grafica en el tipo de meses */
+
+function meses_annos(fechaInicio, fechaFinal, codigoPlaza) {
+   
+    var days = [];
+   
+    for (var w = 0; w < datos.length; w++) {
+
+        var repl = datos[w].ridnombramientos.replace(/[{}]/g, "").split(",");
+
+        if (repl[0] == codigoPlaza || repl[1] == codigoPlaza){
+
+            var fechaN = Date.parse(datos[w].rdia);
+            var fechaInicioE = Date.parse(fechaInicio);
+            var fechaFinalE = Date.parse(fechaFinal);
+
+            if (fechaN >= fechaInicioE && fechaN <= fechaFinalE) {
+                console.log(fechaN);
+                days.push({ "rporcentaje": datos[w].rporcentaje, "rdia": datos[w].rdia })
+
+            }else if(fechaFinal < fechaInicio){
+               alert("Las fechas no coinciden");
+               break;
+            }
+
+        }else if (!datos.ridnombramientos.includes(repl[0]) && !datos.ridnombramientos.includes(repl[1])){
+            alert("EL código de la plaza no existe");
+        }
+
+    }
+
+    var listameses = [];
+        
+    for (var index = 0; index < days.length; index++){
+            var fecha = days[index].rdia.split("-");
+    
+            if (!listameses.includes(fecha[1])){
+                listameses.push(fecha[1]); 
+            }
+     
+    }
+        
+    console.log(listameses)
+       
+    var maxmin = [];
+    
+       for (var i = 0; i < listameses.length; i++){
+    
+           var newlist = []
+           for(var x = 0; x < days.length; x++){
+               
+            var fecha = days[x].rdia.split("-");
+    
+                if (listameses[i] == fecha[1]){
+                    newlist.push({'porc': parseInt(days[x].rporcentaje), 'fecha': days[x].rdia}); 
+                }
+           }
+        
+           var valorMax = getMax(newlist); var valorMin = getMin(newlist);
+    
+           maxmin.push({'mes': listameses[i],'min' : valorMin[0],'fechaMin': valorMin[1],'max':valorMax[0],'fechaMax':valorMax[1] });
+         
+        }
+    
+    console.log(maxmin);
+    pintarGraficoMesesAnnos(maxmin);
+
+}
+/**
+ * 
+ * @param {JSON list} lista 
+ * 
+ * Funcion para obtener el maximo de la lista
+ */
+function getMax(lista) {
+
+    var max = 0;
+    var fechaM;
+
+    for (var x = 0; x < lista.length; x++) {
+        if (lista[x].porc >= max) {
+            max = lista[x].porc;
+            fechaM = lista[x].fecha;
+        }
+    }
+    return [max, fechaM];
+}
+/**
+ * 
+ * @param {JSON list} lista 
+ * 
+ * Funcion para obtener el minimo de la lista
+ */
+function getMin(lista) {
+
+    var min = 100;
+    var fechaM;
+
+    for (var x = 0; x < lista.length; x++) {
+        if (lista[x].porc <= min) {
+            min = lista[x].porc;
+            fechaM = lista[x].fecha;
+        }
+    }
+
+    return [min, fechaM];
+
+}
+
+function calcularTamano(tamano){
+
+
+    
+    var lista = document.getElementsByClassName('barra');
+    for(i = 0; i<lista.length; i++){
+        lista[i].height = tamano;
+        console.log(lista[i].height);
+    }
+    console.log(lista);
+    console.log(tamano);
+
+}
+//-------------------------------------------------------------------------------------------------------
+function pintarGrafico(datosEjeX) {
+
+    console.log(datosEjeX);
+    var objGraficaX = document.getElementById('ejeX');
+    objGraficaX.innerHTML = "";
+
+    
+    for (i = 0; i < datosEjeX.length; i++) {
+        console.log(datosEjeX[i].rporcentaje);
+        var hijo = document.createElement('li');
+        var etiqueta = document.createElement('span');
+        hijo.className = 'barra tooltip';
+        etiqueta.innerText=datosEjeX[i].rdia+"\n"+ datosEjeX[i].rporcentaje+ '%';
+        etiqueta.className = 'tooltiptext';
+
+                
+
+        if (datosEjeX[i].rporcentaje < 100) {
+            hijo.style.backgroundColor = '#FE9A2E';
+            hijo.style.height = 0 + 'px'
+
+        }
+
+        else if (datosEjeX[i].rporcentaje == 100) {
+            hijo.style.backgroundColor = '#AEB404'; //verde
+            hijo.style.height = 0 + 'px'
+        }
+
+        else if (datosEjeX[i].rporcentaje > 100) {
+            hijo.style.backgroundColor = '#FA5858'; //
+            hijo.style.height = 0 + 'px'
+        }
+
+
+        
+        objGraficaX.append(hijo);
+        hijo.style.position = 'relative';
+        hijo.style.top = -(datosEjeX[i].rporcentaje - 25) + 'px'
+        hijo.append(etiqueta);
+        hijo.style.height = datosEjeX[i].rporcentaje + 'px'
+
+        
+        
+    
+    }
+}
+
+function pintarGraficoMesesAnnos(datosEjeX) {
+
+    console.log(datosEjeX);
+    var objGraficaX = document.getElementById('ejeX');
+    objGraficaX.innerHTML = "";
+    
+    for (i = 0; i < datosEjeX.length; i++) {
+        
+        
+        var hijo = document.createElement('li');
+        var hijo2 = document.createElement('li');
+
+        var etiqueta = document.createElement('span');
+        var etiqueta2 = document.createElement('span');
+
+        hijo.className = 'barra tooltip';
+        hijo2.className = 'barra tooltip';
+
+        etiqueta.innerText=datosEjeX[i].fechaMin+"\n"+ datosEjeX[i].min+ '%';
+        etiqueta2.innerText=datosEjeX[i].fechaMax+"\n"+ datosEjeX[i].max+ '%';
+
+        etiqueta.className = 'tooltiptext';
+        etiqueta2.className = 'tooltiptext';
+
+
+
+        if (datosEjeX[i].max < 100 ) {
+            
+            hijo2.style.backgroundColor = '#FE9A2E';
+            hijo2.style.height = 0 + 'px';
+        }
+
+        if (datosEjeX[i].min < 100) {
+            hijo.style.backgroundColor = '#FE9A2E';
+            hijo.style.height = 0 + 'px'
+        }
+
+
+        else if (datosEjeX[i].max == 100 ) {
+            
+            hijo2.style.backgroundColor = '#AEB404'; //verde
+            hijo2.style.height = 0 + 'px'
+        }
+
+        else if (datosEjeX[i].min == 100) {
+            hijo.style.backgroundColor = '#AEB404'; //verde
+            hijo.style.height = 0 + 'px';
+        }
+
+
+        else if (datosEjeX[i].max > 100 ) {
+            hijo2.style.backgroundColor = '#FA5858'; //
+            hijo2.style.height = 0 + 'px'
+        }
+
+        else if ( datosEjeX[i].min > 100) {
+            hijo.style.backgroundColor = '#FA5858'; //
+            hijo.style.height = 0 + 'px'
+        }
+        
+        objGraficaX.append(hijo);
+        objGraficaX.append(hijo2);
+
+        hijo.style.position = 'relative';
+        hijo2.style.position = 'relative';
+        
+        hijo.style.top = -(datosEjeX[i].min - 25) + 'px'
+        hijo2.style.top = -(datosEjeX[i].max - 25) + 'px'
+
+        hijo.append(etiqueta);
+        hijo2.append(etiqueta2);
+
+        hijo.style.height = datosEjeX[i].min + 'px'
+        hijo2.style.height = datosEjeX[i].max + 'px'
+
+        
+        
+    
+    }
+    datosDiasMesesAnnos = [];
+    console.log(datosDiasMesesAnnos);
+}
+
+
+const slider = document.querySelector('.grafico');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+//Cuando se cliquea sobre el grafico estadistico
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+//Cuando se saca el mouse del grafico
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+//Cuando se libera el click
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+
+slider.addEventListener('mousemove', (e) => {
+  //Solo se muev si el click está pulsado
+  if(!isDown) return;
+  e.preventDefault();
+  //Se calcula la posición del mouse con respecto al grafico y la primera posición del mouse al hacer click
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; 
+  //Se establece el valor del slider
+  slider.scrollLeft = scrollLeft - walk;
+});
